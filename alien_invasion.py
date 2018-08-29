8de439c86ef25dfe7b66128b9d2bf49556bc8ee7
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-08-29 14:57:29 -0700

    ships are built

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,14 +5,18 @@ Python 3.6
 import pygame
 import sys
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
     # Initialize game and create a screen object
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_width))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+
+    # Make a ship.
+    ship = Ship(screen)
 
     # Start the main loop for the game
     while True:
@@ -23,6 +27,7 @@ def run_game():
                 sys.exit()
 
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recent drawn screen visible
         pygame.display.flip()
