11fded4c4a184865a8d80d1ccfc8d2c118129981
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-08-29 15:42:41 -0700

    moving left and right

--- game_functions.py
+++ game_functions.py
@@ -6,11 +6,24 @@ import pygame
 import sys
 
 
-def check_events():
+def check_events(ship):
     # Watch for keyboard and mouse events.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # move the ship to the right.
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                # move the ship to the right.
+                ship.moving_left = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
