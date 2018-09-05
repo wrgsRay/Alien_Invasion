5b9d8df49df1359c87a7b82163444630d90ed523
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-05 16:49:22 -0700

    game stops after 3 lives are gone

--- game_stats.py
+++ game_stats.py
@@ -2,10 +2,13 @@
 Python 3.6
 @Author: wrgsRay
 """
-class GameStats():
+
+
+class GameStats:
     def __init__(self, ai_settings):
         self.ai_settings = ai_settings
         self.reset_stats()
+        self.game_active = True
 
     def reset_stats(self):
         # Initialize statistics that can change during the game.
--- settings.py
+++ settings.py
@@ -27,7 +27,7 @@ class Settings:
 
         # Alien settings
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 100
         # fleet_direction of 1 represents right, -1 represents left.
         self.fleet_direction = 1
 
