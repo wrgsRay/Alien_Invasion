45e1ee071dd4f75bb4a9a49f82380d4594beb501
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-12 16:05:53 -0700

    Game's done

--- scoreboard.py
+++ scoreboard.py
@@ -3,6 +3,8 @@ Python 3.6
 @Author: wrgsRay
 """
 import pygame.sysfont
+from pygame.sprite import Group
+from ship import Ship
 
 
 class Scoreboard:
@@ -23,6 +25,7 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         # Turn the score into rendered image.
@@ -40,6 +43,8 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        # Draw ships
+        self.ships.draw(self.screen)
 
     def prep_high_score(self):
         # Turn the high score into a rendered image
@@ -66,6 +71,15 @@ class Scoreboard:
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        # Show many ships are left
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
 
 
 def main():
